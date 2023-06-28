390243373e1b756dfddbef29ff6a96d5fb605ae3
Willson Ghimire <willsonghimire58@gmail.com>
2023-06-28 12:15:46 +0545

    interface complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Fragment } from "react";
 import Header from "./Components/Layout/Header";
+import Meal from "./Components/Meals/Meal";
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meal />
+      </main>
     </Fragment>
   );
 }
